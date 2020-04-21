3044ee451c7555f769facc36a2279e29edcfb85e
Bitajune <beheshtibita@gmail.com>
2020-04-21 12:59:10 -0700

    fixed querySelector

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 let titleEl = document.querySelector("#title")
 console.dir(titleEl);
 titleEl.style.textAlign = "center"
-var pEl = document.querySelector(".cool")
+let pEl = document.querySelector(".cool")
 console.log(pEl)
 pEl.style.color = "red"
-const commentEls = document.querySelectorAll(".comment")
+let commentEls = document.querySelectorAll(".comment")
 console.log(commentEls)
 for (let commentEl of commentEls) {
     commentEl.style.color = "pink"
